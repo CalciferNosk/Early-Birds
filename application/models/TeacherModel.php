28a12e69be0a06cf0553a21c0368d5a533813193
erickson <eadriano.it@gmail.com>
2023-10-19 10:57:49 +0800

    fixed sessions adn add module controller

---
+++ application/models/TeacherModel.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class TeacherModel extends CI_Model
+{
+
+    public function __construct()
+    {   
+        // $this->max_concat = $this->db->query("SET SESSION group_concat_max_len = 18446744073709551615;");
+        parent::__construct();
+    }
+
+    
+}
